a91fe87459a608d74c26b4a38bb2cccd0c99083c
Travis A. Everett <travis.a.everett@gmail.com>
2020-05-03 08:26:42 -0500

    update test script

--- default.nix
+++ default.nix
@@ -11,13 +11,24 @@ pkgs.doxygen.overrideAttrs (attrs: {
   doCheck = true;
   checkPhase = ''
     # rm outputs; not necessary with nix-build but useful with nix-shell
-    rm -rf html latex doxygen_sqlite3.db
+    rm -rf xml html latex doxygen_sqlite3.db
+
     # build process changes directory to doxygen/build
     cd ../
+
+    # for STRIP_FROM_PATH
+    DOXYGEN_ROOT=$(pwd)
+
+    # make the examples
     build/bin/doxygen examples.conf
-    # show that the name is right in XML
-    head -n 20 xml/group__group5_mypage1.xml
-    # test whether it's right in sqlite3 db
+
+    # show the XML
+    head -n -0 xml/index.xml xml/group__group5_*.xml
+
+    # show the sql
+    sqlite3 doxygen_sqlite3.db -column -header ".width 5 5 0 40" "select rowid, kind, refid, name from def"
+
+    # test whether name is right in sqlite3 db
     sqlite3 doxygen_sqlite3.db "select name from def where kind='page' and name='mypage1'" | grep mypage1
 
     # cd back into build so that 'make install' will work and not muddy the water
